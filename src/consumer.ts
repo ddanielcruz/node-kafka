66d9f034614b3a6b8d324242b84a3d0a24167846
Daniel Cruz <danielcunha54@gmail.com>
2022-08-01 19:39:55 -0300

    refactor: simplify scripts

--- src/consumer.ts
+++ src/consumer.ts
@@ -1,13 +1,17 @@
+import { argv } from 'process'
+
 import { kafka } from './kafka'
 
+const groupId = argv[2] || 'consumer'
+
 const program = async () => {
-  const consumer = kafka.consumer({ groupId: 'test-group' })
+  const consumer = kafka.consumer({ groupId })
   await consumer.connect()
   await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       const prefix = `${topic} [${partition} | ${message.offset}] / ${message.timestamp}`
-      console.log(`- [${prefix}] ${message.key || 'empty'}#${message.value}`)
+      console.log(`[${prefix}] ${message.value}`)
     }
   })
 }
--- src/producer.ts
+++ src/producer.ts
@@ -1,23 +1,19 @@
 import { CompressionTypes, Partitioners } from 'kafkajs'
 import { kafka } from './kafka'
 
-const SAMPLE = 10
-
 const program = async () => {
-  console.log(`Sending ${SAMPLE} messages`)
+  console.log(`Sending messages to consumers`)
 
   const producer = kafka.producer({ createPartitioner: Partitioners.DefaultPartitioner })
   await producer.connect()
 
-  for (let idx = 0; idx < SAMPLE; idx++) {
+  setInterval(async () => {
     await producer.send({
       topic: 'test-topic',
-      messages: [{ value: `Message ${idx}!` }],
+      messages: [{ value: `Message ${Date.now()}!` }],
       compression: CompressionTypes.GZIP
     })
-  }
-
-  await producer.disconnect()
+  }, 250)
 }
 
 program()
